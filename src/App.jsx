ca0561deb583f82965291733a11c33f672f5742c
MartinPorciuncula <kgtprav@gmail.com>
2023-08-16 14:55:09 -0300

    correcion de bug visual

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
   }
 
   return (
-    <main className="bg-[url(/images/bg-fscreen.png)] min-h-screen h-full w-full flex flex-col items-center font-Fira">
+    <main className="bg-[url(/images/bg-fscreen.png)] min-h-screen bg-contain w-full flex flex-col items-center font-Fira overflow-hidden">
       <div className="p-6 pb-0">
         <img
           className="max-w-[600px] w-full mx-auto min-[400px]:h-24"
@@ -39,13 +39,13 @@ function App() {
       <div className="w-full rotate-180 min-[400px]:w-[400px]">
         <img src="/images/black-hole-ram.png" />
       </div>
-      <section className="h-[400px] w-10/12 flex justify-center mb-6 -mt-6 relative min-[400px]:w-[280px]">
-        <section className={`absolute ${showCards === idCards.id1 ? 'top-0 opacity-1 visible' : '-top-96 opacity-0 invisible'} transition-[opacity_visibility] duration-1000 w-full`}>
+      <section className={`h-auto w-10/12 flex justify-center mb-6 -mt-6 relative min-[400px]:w-[280px]`}>
+        <section className={`${showCards === idCards.id1 ? 'top-0 opacity-1 visible ml-[480px]' : '-mt-96 opacity-0 invisible'} transition-[opacity_visibility] duration-1000 w-full`}>
           <CharactersCard image={"/images/porci.png"} name={'Martin Porciúncula'} country={'Uruguay'} url={idUrl.id1} />
         </section>
-        <section className={`absolute ${showCards === idCards.id2 ? 'top-0 opacity-1 visible' : '-top-96 opacity-0 invisible'} transition-[opacity_visibility] duration-1000 w-full`}>
+        <section className={` ${showCards === idCards.id2 ? 'top-0 opacity-1 visible' : '-mt-96 opacity-0 invisible'} transition-[opacity_visibility] duration-1000 w-full`}>
           {<CharactersCard image={"/images/rick-morty-robot.png"} name={'Edwar León'} country={'Colombia'} url={idUrl.id2} />}</section>
-        <section className={`absolute ${showCards === idCards.id3 ? 'top-0 opacity-1 visible' : '-top-96 opacity-0 invisible'} transition-[opacity_visibility] duration-1000 w-full`}>
+        <section className={` ${showCards === idCards.id3 ? 'top-0 opacity-1 visible mr-[480px]' : '-mt-96 opacity-0 invisible'}  transition-[opacity_visibility] duration-1000 w-full`}>
           {<CharactersCard image={"/images/tinckles-ram.png"} name={'Alan Polanco'} country={'Argentina'} url={idUrl.id3} />}</section>
       </section>
     </main >
